64298f7c8e6f1b859ba55d5e3dbad0eabd63554e
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-02-06 10:38:22 -0600

    commets

--- src/TestPasswordValidation.java
+++ src/TestPasswordValidation.java
@@ -4,8 +4,10 @@ public class TestPasswordValidation extends TestCase {
 	
 	public void testValidation() {
 		PasswordValidator p = new PasswordValidator();
+		
+		//function takes comments, expected value and the actual function return
 		assertEquals("Passsword should be between 5 - 10 characters",true,p.validate("Prashant"));
 		assertEquals("Passsword should be between 5 - 10 characters",false,p.validate("pass"));
-//		assertEquals("Passsword should be between 5 - 10 characters",false,p.validate("passwordistoolong"));
+		assertEquals("Passsword should be between 5 - 10 characters",false,p.validate("passwordistoolong"));
 	}
 }
